a9cd62594da030c7e3815ae15a51784226c1510e
Bart De Lathouwer <4082369+lathoub@users.noreply.github.com>
2025-06-08 20:03:53 +0200

    remove error logging as it caused exception

--- src/nexa.js
+++ src/nexa.js
@@ -169,22 +169,20 @@ module.exports = {
 			}
 			catch (error)
 			{
-				//self.log("error", `${err}`)
+				self.log("error", `==============> >> ${error}`)
 	
 				if (error.response) {
 					// The request was made and the server responded with a status code
 					// that falls out of the range of 2xx			
-					self.log("error", ` Error: ${error.response.status} Detail: ${error.response.data.detail}`) // detail, status, title
 					self.updateStatus(InstanceStatus.BadConfig)
 				} else if (error.request) {
 					// The request was made but no response was received
 					// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
 					// http.ClientRequest in node.js
-					self.log("error", error.request)
 					self.updateStatus(InstanceStatus.ConnectionFailure)
 				} else {
 					// Something happened in setting up the request that triggered an Error
-					self.log("error", `Error ${error.message}`)
+					//self.log("error", `Error ${error.message}`)
 					self.updateStatus(InstanceStatus.ConnectionFailure)
 				}
 			  
